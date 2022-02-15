58b75dffddb4f3b056aa3405d93ef30a15f04c72
Nisha3001 <Nishanthimuthu99@gmail.com>
2022-02-15 11:17:58 +0530

    message

---
+++ src/org/javaproject/Age.java
@@ -0,0 +1,18 @@
+package org.javaproject;
+
+
+
+public class Age {
+	public static void main(String[] args) {
+		int age =18;
+		if(age>=20) {
+			System.out.println("eligible of aadhar id");
+		}
+		else {
+			System.out.println("not eligible of aadhar id");
+		}
+
+	}		
+		}
+
+
